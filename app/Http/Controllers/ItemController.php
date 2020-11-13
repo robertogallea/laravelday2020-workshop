01d43949abc38d084583944581b501b6e31e4f58
roberto.gallea <write@robertogallea.com>
2020-11-13 10:06:16 +0100

    Initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+
+class ItemController
+{
+    public function __invoke()
+    {
+        $items = Item::all();
+
+        $count = $items->count();
+        $last_update = $items
+            ->sortByDesc
+            ->created_at
+            ->first()
+            ->created_at
+            ->format('d/m/Y');
+
+        $data = [
+            'Numero elementi' => $count,
+            'Ultimo aggiornamento' => $last_update,
+        ];
+
+        return view('index')->with(compact('items', 'data'));
+    }
+}
