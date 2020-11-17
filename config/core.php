2adf3e0767316617620a25c0e078ad0abb235eeb
roberto.gallea <write@robertogallea.com>
2020-11-17 12:24:22 +0100

    Add configuration to CoreServiceProvider

---
+++ config/core.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'repositories' => [
+        'items' => 'Workshop\\DBAccess\\Repositories\\DBItemRepository'
+    ]
+];
--- packages/core/src/CoreServiceProvider.php
+++ packages/core/src/CoreServiceProvider.php
@@ -5,17 +5,30 @@ namespace Workshop\Core;
 
 
 use Illuminate\Support\ServiceProvider;
+use Workshop\DBAccess\Repositories\DBItemRepository;
+use Workshop\Domain\Repositories\ItemRepositoryInterface;
 
 class CoreServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            $this->packagePath() . '/config/core.php' , 'core'
+        );
     }
 
     public function boot()
     {
+        $this->publishes([
+            $this->packagePath() . '/config/core.php' => config_path('core.php'),
+        ]);
+
         $this->loadRoutesFrom($this->packagePath() . '/routes/web.php');
+
+        $this->app->bind(
+            ItemRepositoryInterface::class,
+            config('core.repositories.items')
+        );
     }
 
     private function packagePath()
